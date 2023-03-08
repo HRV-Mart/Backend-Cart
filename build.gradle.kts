8377c0451c3d56e6c336625dfecc624d571cfdb4
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-08 12:42:59 +0530

    Add detekt and jacoco (#10)
    * Added detekt config
    
    * Added detekt
    
    * Added jacoco
    
    * Fixed: Jacoco plugin is missing
    
    * Updated unit test to improve code coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.0.4"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("io.gitlab.arturbosch.detekt") version "1.22.0"
+	id("jacoco")// This is to use Jacoco for coverage testing
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
 }
@@ -24,6 +26,8 @@ dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
+	// Detekt-formatting
+	detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
 }
 
 tasks.withType<KotlinCompile> {
@@ -35,4 +39,32 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	// To run Jacoco Test Coverage Verification
+	finalizedBy("jacocoTestCoverageVerification")
+}
+/*
+* detekt configs*/
+detekt {
+	toolVersion = "1.22.0"
+	config = files("config/detekt/detekt.yml")
+}
+/*
+* Jacoco configs*/
+tasks.jacocoTestCoverageVerification {
+	violationRules {
+		rule {
+			excludes = listOf(
+				"com.hrv.mart.user.repository.UserRepository.kt.*"
+			)
+			limit {
+				minimum = "0.9".toBigDecimal()
+			}
+		}
+	}
+}
+tasks.jacocoTestReport{
+	reports {
+		html.required.set(true)
+		generate()
+	}
 }
