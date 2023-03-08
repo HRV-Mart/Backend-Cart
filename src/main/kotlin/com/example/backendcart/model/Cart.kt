b98d9edaff6e8f743d379be9d10ef74af83eba00
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-08 13:02:54 +0530

    Added cart model

--- src/main/kotlin/com/example/backendcart/model/Cart.kt
+++ src/main/kotlin/com/example/backendcart/model/Cart.kt
@@ -1,4 +1,13 @@
 package com.example.backendcart.model
 
-class Cart {
-}
+import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.mapping.Document
+
+@CompoundIndex(name = "cart_idx", def = "{'userId': 1, 'productId': 1}",
+    unique = true)
+@Document("Cart")
+data class Cart (
+    val userId: String,
+    val productId: String,
+    val quantity: Int
+)
