56a01192bff1ba797b142e82e2aee7191c71e80c
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-08 10:22:31 +0530

    Created empty project

---
+++ src/main/kotlin/com/example/backendcart/BackendCartApplication.kt
@@ -0,0 +1,11 @@
+package com.example.backendcart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendCartApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendCartApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/backendcart/BackendCartApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.backendcart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendCartApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
