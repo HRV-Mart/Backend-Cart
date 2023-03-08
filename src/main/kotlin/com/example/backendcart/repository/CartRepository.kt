f6813ece45a164f93fa662acaf65e14ed103089a
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-08 13:06:59 +0530

    Added repository

--- src/main/kotlin/com/example/backendcart/repository/CartRepository.kt
+++ src/main/kotlin/com/example/backendcart/repository/CartRepository.kt
@@ -1,4 +1,12 @@
 package com.example.backendcart.repository
 
-class CartRepository {
+import com.example.backendcart.model.Cart
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+interface CartRepository : ReactiveMongoRepository<Cart, String> {
+    fun findByUserId(userId: String): Flux<Cart>
+    fun findByUserIdAndProductId(userId: String, productId: String): Mono<Cart>
+    fun existsByUserIdAndProductId(userId: String, productId: String): Mono<Boolean>
 }
