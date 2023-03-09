a5e3e578a725f43767953a25450dab8080ccd77e
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-09 09:19:16 +0530

    Updated repository

--- src/main/kotlin/com/example/backendcart/repository/CartRepository.kt
+++ src/main/kotlin/com/example/backendcart/repository/CartRepository.kt
@@ -2,11 +2,15 @@ package com.example.backendcart.repository
 
 import com.example.backendcart.model.Cart
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
+@Repository
 interface CartRepository : ReactiveMongoRepository<Cart, String> {
     fun findByUserId(userId: String): Flux<Cart>
     fun findByUserIdAndProductId(userId: String, productId: String): Mono<Cart>
     fun existsByUserIdAndProductId(userId: String, productId: String): Mono<Boolean>
+    fun deleteByUserIdAndProductId(userId: String, productId: String): Mono<Void>
+    fun deleteByUserId(userId: String): Flux<Void>
 }
