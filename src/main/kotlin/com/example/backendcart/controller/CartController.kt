bca1001eaee76da1ca547a77e01bf465778ccae5
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-09 10:58:52 +0530

    Added rest endpoints for application

--- src/main/kotlin/com/example/backendcart/controller/CartController.kt
+++ src/main/kotlin/com/example/backendcart/controller/CartController.kt
@@ -1,4 +1,33 @@
 package com.example.backendcart.controller
 
-class CartController {
+import com.example.backendcart.model.Cart
+import com.example.backendcart.service.CartService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/cart")
+class CartController (
+    @Autowired
+    private val cartService: CartService
+)
+{
+    @PostMapping
+    fun addProductInCart(@RequestBody cart: Cart) =
+        cartService.addProductToCart(cart)
+    @GetMapping("/{userId}/{productId}")
+    fun getProductQuantityInCart(@PathVariable productId: String, @PathVariable userId: String) =
+        cartService.getProductQuantityInCart(userId, productId)
+    @GetMapping("/{userId}")
+    fun getUserCart(@PathVariable userId: String) =
+        cartService.getUserCart(userId)
+    @PutMapping
+    fun updateProductQuantity(@RequestBody cart: Cart) =
+        cartService.updateProductQuantity(cart)
+    @DeleteMapping("/{userId}/{productId}")
+    fun deleteProductFromCart(@PathVariable productId: String, @PathVariable userId: String) =
+        cartService.deleteProductFromCart(userId, productId)
+    @DeleteMapping("/{userId}")
+    fun emptyCart(@PathVariable userId: String) =
+        cartService.emptyUserCart(userId)
 }
