631e0cde84b679691b1e6640596f4f2ab612471c
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-10 11:29:01 +0530

    Created test file and added add product to cart unit test

---
+++ src/test/kotlin/com/example/backendcart/controller/CartControllerTest.kt
@@ -0,0 +1,33 @@
+package com.example.backendcart.controller
+
+import com.example.backendcart.model.Cart
+import com.example.backendcart.repository.CartRepository
+import com.example.backendcart.service.CartService
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.mock
+import org.springframework.http.server.reactive.ServerHttpResponse
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+
+
+class CartControllerTest {
+    private val response = mock(ServerHttpResponse::class.java)
+    private val cartRepository = mock(CartRepository::class.java)
+    private val cartService = CartService(cartRepository)
+    private val cartController = CartController(cartService)
+    private val cart = Cart(
+        userId = "User ID",
+        productId = "Product ID",
+        quantity = 2
+    )
+    @Test
+    fun `should add product in cart if it is not present in cart`() {
+        doReturn(Mono.just(cart))
+            .`when`(cartRepository)
+            .insert(cart)
+        StepVerifier.create(cartController.addProductInCart(cart, response))
+            .expectNext("Product added to cart")
+            .verifyComplete()
+    }
+}
