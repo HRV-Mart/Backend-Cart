f7bf43f1eb4bc07d9f728c2ae17cade20437e193
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-11 17:13:17 +0530

    Added more unit tests

--- src/test/kotlin/com/example/backendcart/controller/CartControllerTest.kt
+++ src/test/kotlin/com/example/backendcart/controller/CartControllerTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mockito.doReturn
 import org.mockito.Mockito.mock
 import org.springframework.http.server.reactive.ServerHttpResponse
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
 
@@ -56,4 +57,60 @@ class CartControllerTest {
             .expectNext(0)
             .verifyComplete()
     }
+    @Test
+    fun `should get productId present in user cart`() {
+        doReturn(Flux.just(cart))
+            .`when`(cartRepository)
+            .findByUserId(cart.userId)
+        StepVerifier.create(cartController.getUserCart(cart.userId))
+            .expectNext(cart.productId)
+            .verifyComplete()
+    }
+    @Test
+    fun `should update product quantity when it is available in cart`() {
+        val newCart = Cart(
+            userId = cart.userId,
+            productId = cart.productId,
+            quantity = 20
+        )
+        doReturn(Mono.just(true))
+            .`when`(cartRepository)
+            .existsByUserIdAndProductId(cart.userId, cart.productId)
+        doReturn(Mono.empty<Cart>())
+            .`when`(cartRepository)
+            .deleteByUserIdAndProductId(cart.userId, cart.productId)
+        doReturn(Mono.just(newCart))
+            .`when`(cartRepository)
+            .insert(newCart)
+        StepVerifier.create(cartController.updateProductQuantity(newCart, response))
+            .expectNext("Product quantity updated")
+            .verifyComplete()
+    }
+    @Test
+    fun `should not update product quantity when it is not available in cart`() {
+        val newCart = Cart(
+            userId = cart.userId,
+            productId = cart.productId,
+            quantity = 20
+        )
+        doReturn(Mono.just(false))
+            .`when`(cartRepository)
+            .existsByUserIdAndProductId(cart.userId, cart.productId)
+        StepVerifier.create(cartController.updateProductQuantity(newCart, response))
+            .expectNext("Product not found")
+            .verifyComplete()
+    }
+    @Test
+    fun `should delete product from cart when it is available in cart`() {
+        doReturn(Mono.just(true))
+            .`when`(cartRepository)
+            .existsByUserIdAndProductId(cart.userId, cart.productId)
+        doReturn(Mono.empty<Void>())
+            .`when`(cartRepository)
+            .deleteByUserIdAndProductId(cart.userId, cart.productId)
+        StepVerifier.create(cartController.deleteProductFromCart(cart.productId, cart.userId, response))
+            .expectNext("Product removed from cart")
+            .verifyComplete()
+    }
+
 }
